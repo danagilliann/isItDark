05a39eb455f4ffb929e47d4c4facff697c5f1769
danagilliann <danalee96@gmail.com>
2015-05-13 23:25:20 -0400

    updates with a back button yay

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -12,12 +12,16 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var isItDark: UILabel!
     @IBOutlet weak var findOut: UIButton!
-
+    @IBOutlet weak var backButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        
         self.findOut.setTitle("Find Out!", forState: UIControlState.Normal)
+        self.backButton.setTitle(" ", forState: UIControlState.Normal)
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,9 +29,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func findOutTapped(sender: AnyObject) {
-        self.findOut.setTitle("Yes!", forState: UIControlState.Normal)
+    @IBAction func backButton(sender: AnyObject) {
+        viewDidLoad()
     }
+    @IBAction func findOutTapped(sender: AnyObject) {
+        var time = 18
+        if time >= 18 {
+            self.findOut.setTitle("Yes!", forState: UIControlState.Normal)
+            self.backButton.setTitle("BACK", forState: UIControlState.Normal)
+        } else {
+            self.findOut.setTitle("No.", forState: UIControlState.Normal)
+            self.backButton.setTitle("BACK", forState: UIControlState.Normal)
+        }
+        
+    }
+    
 
 }
 
